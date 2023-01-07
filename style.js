58b9c35785287a4b2c725e258ba464aa8695cbf0
TASOUFIANE <soufiane.taatalla@gmail.com>
2023-01-07 15:56:06 +0100

    retouches header page

--- style.js
+++ style.js
@@ -1,6 +1,10 @@
 
 const navLinks = document.querySelector(".nav-links");
+const uil=document.querySelector(".uil");
 
 function MyFunction(){
+      uil.classList.toggle('uil-multiply');
+      uil.classList.toggle('uil-bars');
       navLinks.classList.toggle('mobile-menu');
+
 }
